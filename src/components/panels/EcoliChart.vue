e62ef9891afb5105704b161099ca84dbf7932046
Peter Finn <firefox0102@gmail.com>
2017-10-29 16:24:14 -0400

    Added "on date" to ecoli chart

--- src/components/panels/EcoliChart.vue
+++ src/components/panels/EcoliChart.vue
@@ -34,10 +34,10 @@
         <span class="graph-card-title__secondary--strong">
           Last Result:
         </span>
-        <span class="graph-card-title__secondary">
-          {{ selectedSite ? selectedSite.lastEColiResult : null }}
-          on 
-          {{ date }}
+        <span v-if="selectedSite" class="graph-card-title__secondary">
+          {{ selectedSite.lastEColiResult }}
+          on
+          {{ selectedSite.lastCollectionDate }}
         </span>
       </div>
     </div>
