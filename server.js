61d0c11d8d8cfce7017cf1e2ffecbdca052b0942
Leslie <lcaceda11@gmail.com>
2017-10-29 22:45:27 -0400

    playing with router

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
+// var history = require('connect-history-api-fallback');
+
+var history = require('connect-history-api-fallback');
+
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+app = express()
+//add this middleware
+app.use(history());
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started '+ port)
